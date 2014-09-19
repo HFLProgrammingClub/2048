e34dad2a35de51f0801f2e403253e82f4d41aed3
blueForestIcarus <erichspaker2@gmail.com>
2014-09-19 10:30:34 -0400

    Update Display.java

--- src/main/Display.java
+++ src/main/Display.java
@@ -7,18 +7,33 @@ import view.Structure;
 import view.View;
 
 public class Display {
+	Board board = new Board();
 	View view = new View();
+	Structure board = new Structure();
+	Structure screen = new Structure();
 	display(){
 		
 	}
 }
 
+//might make this static
 class Board extends Blueprints{
 	
 	Map<String, String> defaults;
 	
-	void board(){
-		defaults = {"position":"relative","posX":"0","posY":"0","height":"100","width","100"}
+	void Board(){
+		defaults = {"position":"relative","posX":"0","posY":"0","height":"100","width":"100"};
+	}
+	
+	void Board(Map<String,String> d){
+		defaults = d;
+	}
+	
+	Structure newBoard(){
+		Structure structure = new Structure();
+		structure.blueprints = this;
+		structure.update();
+		return structure;
 	}
 	
 	public void draw(Structure structure){
