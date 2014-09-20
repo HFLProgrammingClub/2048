33c7d72d9b2992a85c0ac2238800bea85bac9bc2
Wyatt J. Brown <sushidudeteam@gmail.com>
2014-09-20 03:06:59 -0400

    Major refactor: Added gitignore. Added Eclipse formatting and clean up settings. Renamed the package. Deleted unnecessary files.

---
+++ src/com/hflprogramming/espaker16/engine2048/Display.java
@@ -0,0 +1,44 @@
+package com.hflprogramming.espaker16.engine2048;
+
+import java.util.Map;
+
+import view.Blueprints;
+import view.Structure;
+import view.View;
+
+public class Display {
+	Board board = new Board();
+	View view = new View();
+	Structure board = new Structure();
+	Structure screen = new Structure();
+
+	display() {
+
+	}
+}
+
+//might make this static
+class Board extends Blueprints {
+
+	Map<String, String> defaults;
+
+	void Board(){
+		defaults = {"position":"relative","posX":"0","posY":"0","height":"100","width":"100"};
+	}
+
+	void Board(Map<String, String> d) {
+		defaults = d;
+	}
+
+	Structure newBoard() {
+		final Structure structure = new Structure();
+		structure.blueprints = this;
+		structure.update();
+		return structure;
+	}
+
+	@Override
+	public void draw(Structure structure) {
+
+	}
+}
---
+++ src/com/hflprogramming/espaker16/engine2048/Main.java
@@ -0,0 +1,8 @@
+package com.hflprogramming.espaker16.engine2048;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+}
