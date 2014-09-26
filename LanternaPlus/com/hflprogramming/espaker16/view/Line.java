9024089830f92b068daaca89a831af77427240b0
unknown <ffrey15@.hflcampus.monroe.edu>
2014-09-26 14:58:44 -0400

    Hopefully fixed view package

---
+++ LanternaPlus/com/hflprogramming/espaker16/view/Line.java
@@ -0,0 +1,26 @@
+package com.hflprogramming.espaker16.view;
+
+import com.googlecode.lanterna.screen.ScreenCharacter;
+
+public class Line {
+	String pointOne;
+	String pointTwo;
+
+	ScreenCharacter character;
+
+	boolean visible = true;
+
+	public Line(String a, String b, ScreenCharacter c) {
+		pointOne = a;
+		pointTwo = b;
+
+		character = c;
+	}
+
+	public Line(String a, String b, char c) {
+		pointOne = a;
+		pointTwo = b;
+
+		character = new ScreenCharacter(c);
+	}
+}
