f865621207bdb74d8081fbc24caef00d5859da81
Erich Spaker <erichspaker@gmail.com>
2014-09-26 15:21:07 +0000

    fixed things

--- src/com/hflprogramming/espaker16/engine2048/Engine.java
+++ src/com/hflprogramming/espaker16/engine2048/Engine.java
@@ -5,7 +5,11 @@ import java.util.List;
 import java.util.Random;
 
 public class Engine {
-	private int[][] gameboard = new int[4][4];//rows then columns, 0,0 at bottom left corner
+	private int[][] gameboard;
+
+	public Engine() {
+		gameboard = new int[4][4];//rows then columns, 0,0 at bottom left corner
+	}
 
 	public int[][] look() {
 		return gameboard;
@@ -18,39 +22,43 @@ public class Engine {
 	public static final byte D_UP = 2;
 	public static final byte D_RIGHT = 3;
 
+	public void startGame() {
+		final Random random = new Random();
+		final int cell = random.nextInt(16);//Get a random integer between 0(inclusive) and the 16(exclusive)
+		gameboard[cell % 4][cell / 4] = random.nextInt(10) < 9 ? 2 : 4;//Place new tile. There is a one out of ten chance that the tile will be a four instead of a two.
+	}
+
 	//0:invalid, 1:valid, -1:game-over
 	public int swipe(byte direction) {
-		int[][] buffer = gameboard;//Don't know if i need this ( ".clone()" );
-
 		switch (direction) {
 		case D_UP:
 			//two rotations
-			buffer = rotateBoard(2, buffer);
-			buffer = moveDown(buffer);
-			buffer = rotateBoard(2, buffer);
+			gameboard = rotateBoard(2, gameboard);
+			gameboard = moveDown(gameboard);
+			gameboard = rotateBoard(2, gameboard);
 			break;
 
 		case D_DOWN:
 			//no rotation
-			buffer = moveDown(buffer);
+			gameboard = moveDown(gameboard);
 			break;
 
 		case D_LEFT:
 			//three rotations
-			buffer = rotateBoard(3, buffer);
-			buffer = moveDown(buffer);
-			buffer = rotateBoard(1, buffer);
+			gameboard = rotateBoard(3, gameboard);
+			gameboard = moveDown(gameboard);
+			gameboard = rotateBoard(1, gameboard);
 			break;
 
 		case D_RIGHT:
 			//one rotation
-			buffer = rotateBoard(1, buffer);
-			buffer = moveDown(buffer);
-			buffer = rotateBoard(3, buffer);
+			gameboard = rotateBoard(1, gameboard);
+			gameboard = moveDown(gameboard);
+			gameboard = rotateBoard(3, gameboard);
 			break;
 		}
 
-		if (buffer[0][0] == -1) {
+		if (gameboard[0][0] == -1) {
 			//invalid move return false
 			return 0;
 
@@ -60,20 +68,18 @@ public class Engine {
 			final List<Integer> emptyCells = new ArrayList<>(0);
 
 			for (int i = 0; i < 16; i++) {
-				if (buffer[i % 4][i / 4] == 0) {
+				if (gameboard[i % 4][i / 4] == 0) {
 					emptyCells.add(i);
 				}
 			}
 
 			final Random random = new Random();
 			final int cell = emptyCells.get(random.nextInt(emptyCells.size()));//Get a random integer between 0(inclusive) and the number of empty cells(exclusive).
-			buffer[cell % 4][cell / 4] = random.nextInt(10) < 9 ? 2 : 4;//Place new tile. There is a one out of ten chance that the tile will be a four instead of a two.
-
-			gameboard = buffer.clone();//set gameboard to new state
+			gameboard[cell % 4][cell / 4] = random.nextInt(10) < 9 ? 2 : 4;//Place new tile. There is a one out of ten chance that the tile will be a four instead of a two.
 
 			//check for end of game
 			if (emptyCells.size() < 2) {
-				if (isGameOver(buffer)) {
+				if (isGameOver(gameboard)) {
 					onGameOver();
 					return -1;
 				}
@@ -96,7 +102,7 @@ public class Engine {
 	 * helper function written to rotate the buffer board so one function can be used for
 	 * moving tiles (moveDown) and then the board can be rotated back
 	 */
-	private int[][] rotateBoard(int times, int[][] board) {
+	private static int[][] rotateBoard(int times, int[][] board) {
 		final int[][] buffer = new int[4][4];//rows then columns, 0,0 at bottom left corner
 		//will rotate board right "times" times
 
@@ -158,10 +164,10 @@ public class Engine {
 		}
 	}
 
-	private boolean isGameOver(int[][] buffer) {
+	private static boolean isGameOver(int[][] board) {
 		for (int row = 0; row < 3; row++) {
 			for (int col = 0; col < 3; col++) {
-				if (buffer[row][col] == buffer[row][col + 1]) {
+				if (board[row][col] == board[row][col + 1]) {
 					return false;
 				}
 			}
@@ -169,7 +175,7 @@ public class Engine {
 
 		for (int col = 0; col < 3; col++) {
 			for (int row = 0; row < 3; row++) {
-				if (buffer[row][col] == buffer[row + 1][col]) {
+				if (board[row][col] == board[row + 1][col]) {
 					return false;
 				}
 			}
--- src/com/hflprogramming/espaker16/engine2048/Main.java
+++ src/com/hflprogramming/espaker16/engine2048/Main.java
@@ -2,7 +2,31 @@ package com.hflprogramming.espaker16.engine2048;
 
 public class Main {
 
+	static Engine engine;
+	static Display display;
+
 	public static void main(String[] args) {
-		new Display();
+		display = new Display();
+		engine = new Engine();
+
+		engine.startGame();
+
+	}
+
+	public void gameloop() {
+
+		while (true) {
+			for (byte dir = 1; dir <= 4; dir++) {
+				final int status = engine.swipe(dir);
+				if (status == 0) {
+					continue;
+				} else if (status == 1) {
+					break;
+				} else {
+					return;
+				}
+			}
+
+		}
 	}
 }
