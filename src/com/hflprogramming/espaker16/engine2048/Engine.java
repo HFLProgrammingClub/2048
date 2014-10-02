a1763fa709dbaca5dcd73b4dfe66743cdbec930c
blueForestIcarus <erichspaker2@gmail.com>
2014-10-01 20:53:17 -0400

    Bug Fixes
    fixed bugs in tile merging, rotation, and game over checking

--- src/com/hflprogramming/espaker16/engine2048/Engine.java
+++ src/com/hflprogramming/espaker16/engine2048/Engine.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Random;
 
 public class Engine {
-	private int[][] gameboard;
+	int[][] gameboard;
 	public Display display;
 
 	public Engine() {
@@ -18,9 +18,9 @@ public class Engine {
 
 	public int score;
 
-	public static final byte D_LEFT = 0;
-	public static final byte D_DOWN = 1;
-	public static final byte D_UP = 2;
+	public static final byte D_LEFT = 2;
+	public static final byte D_DOWN = 0;
+	public static final byte D_UP = 1;
 	public static final byte D_RIGHT = 3;
 
 	public void startGame() {
@@ -32,67 +32,91 @@ public class Engine {
 	//0:invalid, 1:valid, -1:game-over
 	public int swipe(byte direction) {
 		switch (direction) {
-		case D_UP:
+		case D_RIGHT:
 			//two rotations
-			gameboard = rotateBoard(2, gameboard);
-			gameboard = moveDown(gameboard);
-			gameboard = rotateBoard(2, gameboard);
-			break;
-
-		case D_DOWN:
-			//no rotation
-			gameboard = moveDown(gameboard);
+			gameboard = rotateBoard(1, gameboard);
+			gameboard = rotateBoard(1, gameboard);
+			try {
+				gameboard = moveDown(gameboard);
+			} catch (final Exception e) {
+				//invalid move return false
+				return 0;
+			}
+			gameboard = rotateBoard(1, gameboard);
+			gameboard = rotateBoard(1, gameboard);
 			break;
 
 		case D_LEFT:
+			//no rotation
+			try {
+				gameboard = moveDown(gameboard);
+			} catch (final Exception e) {
+				//invalid move return false
+				return 0;
+			}
+			break;
+
+		case D_DOWN:
 			//three rotations
-			gameboard = rotateBoard(3, gameboard);
-			gameboard = moveDown(gameboard);
+			gameboard = rotateBoard(1, gameboard);
+			gameboard = rotateBoard(1, gameboard);
+			gameboard = rotateBoard(1, gameboard);
+			try {
+				gameboard = moveDown(gameboard);
+			} catch (final Exception e) {
+				//invalid move return false
+				return 0;
+			}
 			gameboard = rotateBoard(1, gameboard);
 			break;
 
-		case D_RIGHT:
+		case D_UP:
 			//one rotation
 			gameboard = rotateBoard(1, gameboard);
-			gameboard = moveDown(gameboard);
-			gameboard = rotateBoard(3, gameboard);
+			try {
+				gameboard = moveDown(gameboard);
+			} catch (final Exception e) {
+				//invalid move return false
+				return 0;
+			}
+			gameboard = rotateBoard(1, gameboard);
+			gameboard = rotateBoard(1, gameboard);
+			gameboard = rotateBoard(1, gameboard);
 			break;
 		}
 
-		if (gameboard[0][0] == -1) {
-			//invalid move return false
-			return 0;
+		//add random tile to board;
+		//get all empty tiles (there will be at least one)
+		final List<Integer> emptyCells = new ArrayList<>(0);
 
-		} else {
-			//add random tile to board;
-			//get all empty tiles (there will be at least one)
-			final List<Integer> emptyCells = new ArrayList<>(0);
-
-			for (int i = 0; i < 16; i++) {
-				if (gameboard[i % 4][i / 4] == 0) {
-					emptyCells.add(i);
-				}
+		for (int i = 0; i < 16; i++) {
+			if (gameboard[i % 4][i / 4] == 0) {
+				emptyCells.add(i);
 			}
-
-			final Random random = new Random();
-			final int cell = emptyCells.get(random.nextInt(emptyCells.size()));//Get a random integer between 0(inclusive) and the number of empty cells(exclusive).
-			gameboard[cell % 4][cell / 4] = random.nextInt(10) < 9 ? 2 : 4;//Place new tile. There is a one out of ten chance that the tile will be a four instead of a two.
-
-			//check for end of game
-			if (emptyCells.size() < 2) {
-				if (isGameOver(gameboard)) {
-					onGameOver();
-					return -1;
-				}
-			}
-
-			onMove();
-			return 1;
 		}
+
+		final Random random = new Random();
+		final int cell = emptyCells.get(random.nextInt(emptyCells.size()));//Get a random integer between 0(inclusive) and the number of empty cells(exclusive).
+		gameboard[cell % 4][cell / 4] = random.nextInt(10) < 9 ? 2 : 4;//Place new tile. There is a one out of ten chance that the tile will be a four instead of a two.
+
+		//check for end of game
+		if (emptyCells.size() < 2) {
+			if (isGameOver(gameboard)) {
+				onGameOver();
+				return -1;
+			}
+		}
+
+		onMove();
+		return 1;
+
 	}
 
 	private void onGameOver() {
 		// TODO: add onGameOver() functionality
+		updateDisplay();
+		System.out.print("game over");
+
 	}
 
 	private void onMove() {
@@ -102,12 +126,15 @@ public class Engine {
 	/*
 	 * helper function written to rotate the buffer board so one function can be used for
 	 * moving tiles (moveDown) and then the board can be rotated back
+	 * 
+	 * THIS HAS ISSUES do not pass any number more than one for times
 	 */
-	private static int[][] rotateBoard(int times, int[][] board) {
+	static int[][] rotateBoard(int ONLYUSEONEORELSETHINGSBREAK, int[][] board) {
+		//TODO fix multible rotations funtionality
 		final int[][] buffer = new int[4][4];//rows then columns, 0,0 at bottom left corner
 		//will rotate board right "times" times
 
-		for (int i = 0; i < times; i++) {
+		for (int i = 0; i < ONLYUSEONEORELSETHINGSBREAK; i++) {
 			for (int row = 0; row < 4; row++) {
 				//for each row, copy contents to opposite column (row 0 goes to column 3, row 2 goes to column 1 )
 				for (int col = 0; col < 4; col++) {
@@ -115,14 +142,15 @@ public class Engine {
 					buffer[row][col] = board[col][3 - row];
 				}
 				//finally set the board to buffer for next rotation
-				board = buffer;
+				//this breaks everything next line breaks everything
+				//board = buffer.clone();
 			}
 		}
 		return buffer;
 	}
 
 	//helper function for sliding and merging the tiles down
-	private int[][] moveDown(int[][] board) {
+	private int[][] moveDown(int[][] board) throws Exception {
 		boolean valid = false;
 
 		for (int row = 0; row < 4; row++) {
@@ -145,7 +173,8 @@ public class Engine {
 						board[row][b] = 0;
 
 						valid = true;
-
+					} else if (nblock != 0) {
+						break;
 					} else {
 						continue;
 					}
@@ -159,24 +188,15 @@ public class Engine {
 			return board;
 
 		} else {
-			//set board[0][0] to -1 to signify an invalid board move (aka: nothing moves)
-			board[0][0] = -1;
-			return board;
+			//throw to signify an invalid board move (aka: nothing moves)
+			throw new Exception();
 		}
 	}
 
 	private static boolean isGameOver(int[][] board) {
 		for (int row = 0; row < 3; row++) {
 			for (int col = 0; col < 3; col++) {
-				if (board[row][col] == board[row][col + 1]) {
-					return false;
-				}
-			}
-		}
-
-		for (int col = 0; col < 3; col++) {
-			for (int row = 0; row < 3; row++) {
-				if (board[row][col] == board[row + 1][col]) {
+				if (board[row][col] == board[row][col + 1] || board[row][col] == board[row + 1][col]) {
 					return false;
 				}
 			}
