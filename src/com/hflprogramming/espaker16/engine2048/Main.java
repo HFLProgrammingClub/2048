af7decc6b988054235418ab2cb7ab90b71176909
blueForestIcarus <erichspaker2@gmail.com>
2014-10-01 20:53:53 -0400

    Added test code

--- src/com/hflprogramming/espaker16/engine2048/Main.java
+++ src/com/hflprogramming/espaker16/engine2048/Main.java
@@ -1,9 +1,11 @@
 package com.hflprogramming.espaker16.engine2048;
 
+import java.util.Scanner;
 
 public class Main {
 
 	static Engine engine;
+	static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		engine = new Engine();
@@ -11,22 +13,40 @@ public class Main {
 
 		engine.startGame();
 
-		//gameloop();
+		engine.swipe(engine.D_DOWN);
+
+		/*
+		 * test code for playing manually (with numbers entered into console)
+		while (true) {
+			final byte i = (byte) scanner.nextInt();
+			if (i >= 0 && i < 4) {
+				if (engine.swipe(i) == 1) {
+					System.out.println("swipe");
+				}
+			}
+		}
+		*/
+
+		try {
+			gameloop();
+		} catch (final Exception e) {
+			// TODO Auto-generated catch block
+		}
 
 	}
 
-	public static void gameloop() {
+	public static void gameloop() throws Exception {
 		System.out.println("started gameloop");
 
-		boolean x = Math.random() > .5 ? true : false;
-		boolean y = Math.random() > .5 ? true : false;
-		while ((x && y) || !(x && y)) {
+		final boolean x = Math.random() > .5 ? true : false;
+		final boolean y = Math.random() > .5 ? true : false;
+		while (x && y || !(x && y)) {
 			for (byte dir = 1; dir <= 4; dir++) {
 				final int status = engine.swipe(dir);
 
 				if (status == 0) {
 					try {
-						Thread.currentThread().sleep(100);//sleep for 1000 ms
+						Thread.currentThread().sleep(80);//sleep for 1000 ms
 					} catch (final InterruptedException e) {
 						e.printStackTrace();
 					}
@@ -36,7 +56,7 @@ public class Main {
 					break;
 
 				} else {
-					return;
+					throw new Exception();
 				}
 			}
 		}
