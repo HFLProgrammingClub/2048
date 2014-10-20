bbaab3a8934237b0bc4d4c16e5f4b75fddc5cbc2
blueForestIcarus <erichspaker2@gmail.com>
2014-10-20 09:44:58 -0400

    Update Engine.java

--- src/com/hflprogramming/espaker16/engine2048/Engine.java
+++ src/com/hflprogramming/espaker16/engine2048/Engine.java
@@ -17,41 +17,6 @@ public class Engine {
 	public static final byte D_UP = 1;
 	public static final byte D_RIGHT = 3;
 
-	//TODO fix undo code
-	/*
-	public List<int[][]> history = new ArrayList<int[][]>();
-	public int current = 0;
-
-	void loadHistory(int index) {
-		current = index;
-		if (current < 0) {
-			current = 0;
-		}
-		if (current >= history.size()) {
-			current = history.size() - 1;
-		}
-		gameboard = history.get(current);
-		updateDisplay();
-	}
-
-	void undo() {
-		loadDisplay(current+1);
-	}
-
-	void redo() {
-		loadDisplay(current-1);
-	}
-
-	void reset() {
-		if (current != 0) {
-			for (int i = 0; i < current; i++) {
-				history.remove(i);
-				current = 0;
-			}
-		}
-		history.add(0, gameboard);
-	}
-	 */
 	public Engine() {
 		gameboard = new int[4][4];//rows then columns, 0,0 at bottom left corner
 		for (int col = 0; col < 4; col++) {
@@ -66,6 +31,18 @@ public class Engine {
 		final int cell = random.nextInt(16);//Get a random integer between 0(inclusive) and the 16(exclusive)
 		gameboard[cell % 4][cell / 4] = random.nextInt(10) < 9 ? 2 : 4;//Place new tile. There is a one out of ten chance that the tile will be a four instead of a two.
 
+		final List<Integer> emptyCells = new ArrayList<>(0);
+
+		for (int i = 0; i < 16; i++) {
+			if (gameboard[i % 4][i / 4] == 0) {
+				emptyCells.add(i);
+			}
+		}
+
+		final Random random = new Random();
+		final int cell = emptyCells.get(random.nextInt(emptyCells.size()));//Get a random integer between 0(inclusive) and the number of empty cells(exclusive).
+		gameboard[cell % 4][cell / 4] = random.nextInt(10) < 9 ? 2 : 4;//Place new tile. There is a one out of ten chance that the tile will be a four instead of a two
+		
 		if (doDisplay) {
 			updateDisplay();
 		}
